c86405d2c5f2af0aef94a57948878b6a6957c64d
Ezekiel Correa <saints.correa23@gmail.com>
2025-12-22 10:46:37 -0600

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,12 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const ALLOWED_ORIGINS = new Set([
   const ALLOWED_ORIGINS = new Set([
      "https://elspaniard97.github.io",
-     "http://localhost:5500"
+     "http://localhost:5500",
+     "http://localhost:3000",
+     "http://localhost:8080",
+     "http://127.0.0.1:5500",
+     "http://127.0.0.1:3000",
+     "http://127.0.0.1:8080"
 ]);
 
 app.use(
